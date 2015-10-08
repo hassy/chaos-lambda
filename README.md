d1068e8bc6605185f28eec4a0b324bb49ea6ba19
hveldstra <h@veldstra.org>
2015-10-08 13:36:26 +0100

    Chaos Lambda

---
+++ README.md
@@ -0,0 +1,7 @@
+# Chaos Lambda
+
+Netflix's [Chaos Monkey](https://github.com/Netflix/SimianArmy) rebuilt with 2015 technology.
+
+**DO NOT RUN THIS AS-IS**
+
+This is an early WIP. It will kill an instance at random when run. There is no logic beyond that yet.
---
+++ chaos.js
@@ -0,0 +1,28 @@
+// See LICENSE-APL2 for terms
+
+var AWS = require('aws-sdk');
+AWS.config.region = 'eu-west-1';
+
+exports.handler = function(event, context) {
+  var ec2 = new AWS.EC2();
+  ec2.describeInstances(function(err, data) {
+    if (err) {
+      context.done(err, null);
+    }
+
+    var numInstances = data.Reservations[0].Instances.length;
+
+    var random = Math.floor(Math.random() * numInstances);
+    var target = data.Reservations[0].Instances[random];
+    console.log('Going to terminate instance with id = %s', target.InstanceId);
+
+    ec2.terminateInstances({InstanceIds:[target.InstanceId]}, function(err, data) {
+      if (err) {
+        context.done(err, null);
+      }
+
+      console.log('Instance %s terminated', target.InstanceId);
+      context.done(null, data);
+    });
+  });
+};
